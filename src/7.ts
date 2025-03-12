71b636c40d0bd3f69c06137bfdec47dfc5518010
leeremas82 <leeremas82@gmail.com>
2025-03-12 00:07:03 +0000

    Add 7.ts

---
+++ src/7.ts
@@ -0,0 +1,11 @@
+const randomTsCode = `
+class Math {
+  static readonly e: number = 2.718281828459045;
+  static readonly pi: number = 3.141592653589793;
+  static random(max: number): number {
+    return Math.random() * max;
+  }
+}
+`;
+
+export default randomTsCode;
