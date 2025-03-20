29656b2758354b34b4eaf72f08b7f4503fad752e
leeremas82 <leeremas82@gmail.com>
2025-03-20 17:22:24 +0000

    Add 15.ts

---
+++ src/15.ts
@@ -0,0 +1,8 @@
+def calculate_square_root(num):
+    """
+    This function takes an integer num as input and returns its square root.
+    """
+    return num ** 0.5
+
+result = calculate_square_root(16)
+print(result)
